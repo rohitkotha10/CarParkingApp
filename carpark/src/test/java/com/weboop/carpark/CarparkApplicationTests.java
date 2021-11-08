a85d91bf92c5789226e9bc545f99f3551b2cb470
RohitKotha10 <rohitkotha10@gmail.com>
2021-11-08 21:50:23 +0530

    First Issue

---
+++ carpark/src/test/java/com/weboop/carpark/CarparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weboop.carpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
